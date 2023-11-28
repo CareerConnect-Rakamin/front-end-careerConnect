340fa1841ff573f2a2242c1addea7f0974fa43ef
Inal Mahpud <inalmahpud1@gmail.com>
2023-11-28 16:08:24 +0700

    feat: add function update Job

--- src/modules/fetch/index.js
+++ src/modules/fetch/index.js
@@ -16,6 +16,82 @@ async function getCompanyJobs(id) {
     throw new Error(error.response.data.message || 'Something went wrong');
   }
 }
+async function getJobById(id) {
+  try {
+    const response = await instance.get(`/jobs/${id}`);
+    return response.data;
+  } catch (error) {
+    throw new Error(error.response.data.message || 'Something went wrong');
+  }
+}
+
+async function getApllicants(id) {
+  try {
+    const response = await instance.get(`/apply/job/${id}`);
+    return response.data;
+  } catch (error) {
+    return;
+  }
+}
+
+async function updateJob(
+  id,
+  {
+    name,
+    description,
+    what_will_you_do,
+    what_will_you_need,
+    location,
+    category,
+    job_type,
+    salary,
+    capacity,
+    is_open
+  }
+) {
+  console.log(
+    id,
+    name,
+    description,
+    what_will_you_do,
+    what_will_you_need,
+    location,
+    category,
+    job_type,
+    salary,
+    capacity,
+    is_open
+  );
+  try {
+    const response = await instance.put(`/jobs/${id}`, {
+      name,
+      description,
+      what_will_you_do,
+      what_will_you_need,
+      location,
+      category,
+      job_type,
+      salary,
+      capacity,
+      is_open
+    });
+    return response.data;
+  } catch (error) {
+    throw new Error(error.response.data.message || 'Something went wrong');
+  }
+}
+
+async function updateStatusApplicants(id, status, seekerId) {
+  try {
+    const response = await instance.put(`/apply/company/job/${id}`, {
+      status,
+      seekerId
+    });
+    return response.data;
+  } catch (error) {
+    throw new Error(error.response.data.message || 'Something went wrong');
+  }
+}
 
 async function editCompany(
   id,
@@ -60,4 +136,13 @@ async function UpdatePhoto(formData) {
   }
 }
 
-export { getCompanyById, getCompanyJobs, editCompany, UpdatePhoto };
+export {
+  getCompanyById,
+  getCompanyJobs,
+  editCompany,
+  UpdatePhoto,
+  getJobById,
+  getApllicants,
+  updateStatusApplicants,
+  updateJob
+};
