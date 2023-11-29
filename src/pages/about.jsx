09fd65e3a0e15c834100877ce416b3fdaf24bd17
bhanurz <aswanggab@gmail.com>
2023-11-29 10:23:57 +0700

    feat: add few section for about page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,5 +1,18 @@
 import Wrapper from '@/components/Wrapper';
-import { Box, Flex, Image, SimpleGrid, Stack, Text } from '@chakra-ui/react';
+import {
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
+  Avatar,
+  Box,
+  Flex,
+  Image,
+  SimpleGrid,
+  Stack,
+  Text
+} from '@chakra-ui/react';
 import Head from 'next/head';
 
 export default function about() {
@@ -52,6 +65,91 @@ export default function about() {
           />
         </Flex>
       </Stack>
+      <Stack padding={10} bg={'#f8f8f8'}>
+        <Text
+          fontSize={'3xl'}
+          fontWeight={'bold'}
+          fontFamily={'lexendDeca'}
+          color={'blue.400'}
+          textAlign={'center'}
+        >
+          Apa yang Membedakan Kami?
+        </Text>
+        <SimpleGrid p={10} columns={3} gap={5}>
+          <Box h={'300px'} boxShadow={'xl'} rounded={'xl'} bg={'white'}>
+            <Flex direction={'column'} m={6} fontFamily={'lexendDeca'}>
+              <Text
+                textAlign={'center'}
+                fontSize={'2xl'}
+                fontWeight={'semibold'}
+                color={'blue.300'}
+                mb={5}
+              >
+                Teknologi Canggih
+              </Text>
+              <Text textAlign={'justify'} color={'blue.800'}>
+                CareerConnect juga memanfaatkan teknologi terkini untuk
+                menyajikan pengalaman pencarian pekerjaan yang lebih cerdas dan
+                efektif. Dengan sistem pencocokan yang pintar, kami memastikan
+                setiap pencari kerja dan perusahaan memiliki peluang terbaik
+                untuk saling berpadu.
+              </Text>
+            </Flex>
+          </Box>
+          <Box h={'300px'} boxShadow={'xl'} rounded={'xl'} bg={'white'}>
+            <Flex direction={'column'} m={6} fontFamily={'lexendDeca'}>
+              <Text
+                textAlign={'center'}
+                fontSize={'2xl'}
+                fontWeight={'semibold'}
+                color={'blue.300'}
+                mb={5}
+              >
+                Kemitraan Bersama
+              </Text>
+              <Text textAlign={'justify'} color={'blue.800'}>
+                CareerConnect membangun kemitraan dengan berbagai perusahaan
+                untuk memastikan bahwa kami memiliki akses terhadap beragam
+                kesempatan pekerjaan. Kami berkomitmen untuk menjadi penghubung
+                yang handal antara pencari kerja dan perusahaan.
+              </Text>
+            </Flex>
+          </Box>
+          <Box h={'300px'} boxShadow={'xl'} rounded={'xl'} bg={'white'}>
+            <Flex direction={'column'} m={6} fontFamily={'lexendDeca'}>
+              <Text
+                textAlign={'center'}
+                fontSize={'2xl'}
+                fontWeight={'semibold'}
+                color={'blue.300'}
+                mb={5}
+              >
+                Keamanan dan Privasi
+              </Text>
+              <Text textAlign={'justify'} color={'blue.800'}>
+                Keamanan dan privasi informasi pengguna adalah prioritas utama
+                kami. Kami mengambil langkah-langkah untuk memastikan bahwa
+                setiap data yang dibagikan dengan kami dikelola dengan aman dan
+                sesuai dengan standar privasi tertinggi.
+              </Text>
+            </Flex>
+          </Box>
+        </SimpleGrid>
+        <Flex justifyContent={'center'}>
+          <Text
+            fontSize={'md'}
+            fontWeight={'bold'}
+            fontFamily={'lexendDeca'}
+            color={'blue.700'}
+            textAlign={'center'}
+            w={'700px'}
+          >
+            Bergabunglah dengan CareerConnect dan mulailah perjalanan Anda
+            menuju karir yang sukses! Kami berkomitmen untuk membantu Anda
+            mencapai impian dan aspirasi karir Anda.
+          </Text>
+        </Flex>
+      </Stack>
       <Stack padding={10}>
         <Flex
           justifyContent={'space-around'}
@@ -92,6 +190,114 @@ export default function about() {
           </Flex>
         </Flex>
       </Stack>
+      <Stack padding={10}>
+        <Text
+          fontSize={'3xl'}
+          fontWeight={'bold'}
+          fontFamily={'lexendDeca'}
+          color={'blue.400'}
+          textAlign={'center'}
+        >
+          Bagaimana CareerConnect Bekerja?
+        </Text>
+        <Flex justifyContent={'center'} mt={5}>
+          <Accordion
+            allowMultiple
+            w={'700px'}
+            boxShadow={'xl'}
+            rounded={'xl'}
+            fontFamily={'lexendDeca'}
+          >
+            <AccordionItem>
+              <h2>
+                <AccordionButton>
+                  <Box
+                    as="span"
+                    flex="1"
+                    textAlign="left"
+                    fontWeight={'semibold'}
+                    color={'blue.700'}
+                  >
+                    Daftar Akun
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+              </h2>
+              <AccordionPanel pb={4} fontWeight={'light'}>
+                Pendaftaran di CareerConnect adalah langkah pertama Anda menuju
+                peluang karier yang tak terbatas. Isi profil Anda dengan
+                informasi lengkap dan rinci.
+              </AccordionPanel>
+            </AccordionItem>
+
+            <AccordionItem>
+              <h2>
+                <AccordionButton>
+                  <Box
+                    as="span"
+                    flex="1"
+                    textAlign="left"
+                    fontWeight={'semibold'}
+                    color={'blue.700'}
+                  >
+                    Pencarian Pekerjaan
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+              </h2>
+              <AccordionPanel pb={4} fontWeight={'light'}>
+                Gunakan fitur pencarian kami untuk menemukan pekerjaan sesuai
+                kriteria Anda. Filter berdasarkan posisi dan lokasi yang Anda
+                miliki.
+              </AccordionPanel>
+            </AccordionItem>
+
+            <AccordionItem>
+              <h2>
+                <AccordionButton>
+                  <Box
+                    as="span"
+                    flex="1"
+                    textAlign="left"
+                    fontWeight={'semibold'}
+                    color={'blue.700'}
+                  >
+                    Melamar Pekerjaan
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+              </h2>
+              <AccordionPanel pb={4} fontWeight={'light'}>
+                Ajukan lamaran dengan mudah melalui platform kami. Kirimkan CV
+                dan surat lamaran Anda tanpa repot.
+              </AccordionPanel>
+            </AccordionItem>
+
+            <AccordionItem>
+              <h2>
+                <AccordionButton>
+                  <Box
+                    as="span"
+                    flex="1"
+                    textAlign="left"
+                    fontWeight={'semibold'}
+                    color={'blue.700'}
+                  >
+                    Rekrutmen Efisien
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+              </h2>
+              <AccordionPanel pb={4} fontWeight={'light'}>
+                Perusahaan juga dapat memanfaatkan CareerConnect untuk mencari
+                calon karyawan yang ideal. Gunakan fitur rekrutmen kami untuk
+                menyaring pelamar dan mengelola proses seleksi dengan lebih
+                efisien.
+              </AccordionPanel>
+            </AccordionItem>
+          </Accordion>
+        </Flex>
+      </Stack>
       <Stack padding={10} bg={'#f8f8f8'}>
         <Text
           fontSize={'3xl'}
@@ -100,56 +306,214 @@ export default function about() {
           color={'blue.400'}
           textAlign={'center'}
         >
-          Apa yang Membedakan Kami?
+          Meet Our Team
         </Text>
-        <SimpleGrid p={10} columns={3} gap={5}>
-          <Box h={'300px'} boxShadow={'xl'} rounded={'xl'} bg={'white'}>
-            <Flex direction={'column'} m={6} fontFamily={'lexendDeca'}>
-              <Text
-                textAlign={'center'}
-                fontSize={'2xl'}
-                fontWeight={'semibold'}
-                color={'blue.300'}
-                mb={5}
-              >
-                Teknologi Canggih
+        <Flex justifyContent={'center'} alignItems={'center'} gap={4} m={5}>
+          <Box
+            boxShadow={'xl'}
+            width={'260px'}
+            height={'260px'}
+            rounded={'lg'}
+            fontFamily={'lexendDeca'}
+          >
+            <Flex justifyContent={'center'} mt={8}>
+              <Avatar
+                src="/images/team/daffa.jpg"
+                size={'xl'}
+                justifyContent={'center'}
+              />
+            </Flex>
+            <Flex
+              flexDirection={'column'}
+              justifyContent={'center'}
+              alignItems={'center'}
+              p={5}
+            >
+              <Text textAlign={'center'} fontWeight={'semibold'} mb={4}>
+                Muhammad Daffa Satria Negara
               </Text>
-              <Text textAlign={'justify'} color={'blue.800'}>
-                CareerConnect juga memanfaatkan teknologi terkini untuk
-                menyajikan pengalaman pencarian pekerjaan yang lebih cerdas dan
-                efektif. Dengan sistem pencocokan yang pintar, kami memastikan
-                setiap pencari kerja dan perusahaan memiliki peluang terbaik
-                untuk saling berpadu.
+              <Text textAlign={'center'} fontWeight={'light'} fontSize={'sm'}>
+                Fullstack Web Developer
               </Text>
             </Flex>
           </Box>
-          <Box h={'300px'} boxShadow={'xl'} rounded={'xl'} bg={'white'}>
-            <Text
-              textAlign={'center'}
-              fontSize={'2xl'}
-              fontFamily={'lexendDeca'}
-              fontWeight={'semibold'}
-              color={'blue.400'}
-              mt={4}
+
+          <Box
+            boxShadow={'xl'}
+            width={'260px'}
+            height={'260px'}
+            rounded={'lg'}
+            fontFamily={'lexendDeca'}
+          >
+            <Flex justifyContent={'center'} mt={8}>
+              <Avatar
+                src="/images/team/inal.jpg"
+                size={'xl'}
+                justifyContent={'center'}
+              />
+            </Flex>
+            <Flex
+              flexDirection={'column'}
+              justifyContent={'center'}
+              alignItems={'center'}
+              p={5}
             >
-              Kemitraan Bersama
-            </Text>
+              <Text textAlign={'center'} fontWeight={'semibold'} mb={4}>
+                Inal Mahpud
+              </Text>
+              <Text textAlign={'center'} fontWeight={'light'} fontSize={'sm'}>
+                Fullstack Web Developer
+              </Text>
+            </Flex>
           </Box>
-          <Box h={'300px'} boxShadow={'xl'} rounded={'xl'} bg={'white'}>
-            <Text
-              textAlign={'center'}
-              fontSize={'2xl'}
-              fontFamily={'lexendDeca'}
-              fontWeight={'semibold'}
-              color={'blue.400'}
-              mt={4}
+
+          <Box
+            boxShadow={'xl'}
+            width={'260px'}
+            height={'260px'}
+            rounded={'lg'}
+            fontFamily={'lexendDeca'}
+          >
+            <Flex justifyContent={'center'} mt={8}>
+              <Avatar
+                src="/images/team/bagus.jpeg"
+                size={'xl'}
+                justifyContent={'center'}
+              />
+            </Flex>
+            <Flex
+              flexDirection={'column'}
+              justifyContent={'center'}
+              alignItems={'center'}
+              p={5}
             >
-              Keamanan dan Privasi
-            </Text>
+              <Text textAlign={'center'} fontWeight={'semibold'} mb={4}>
+                Bagus Dwi Jauhari
+              </Text>
+              <Text textAlign={'center'} fontWeight={'light'} fontSize={'sm'}>
+                Fullstack Web Developer
+              </Text>
+            </Flex>
           </Box>
-        </SimpleGrid>
+
+          <Box
+            boxShadow={'xl'}
+            width={'260px'}
+            height={'260px'}
+            rounded={'lg'}
+            fontFamily={'lexendDeca'}
+          >
+            <Flex justifyContent={'center'} mt={8}>
+              <Avatar
+                src="/images/team/erza.jpg"
+                size={'xl'}
+                justifyContent={'center'}
+              />
+            </Flex>
+            <Flex
+              flexDirection={'column'}
+              justifyContent={'center'}
+              alignItems={'center'}
+              p={5}
+            >
+              <Text textAlign={'center'} fontWeight={'semibold'} mb={4}>
+                Erza Ikhsani
+              </Text>
+              <Text textAlign={'center'} fontWeight={'light'} fontSize={'sm'}>
+                Fullstack Web Developer
+              </Text>
+            </Flex>
+          </Box>
+        </Flex>
+        <Flex justifyContent={'center'} alignItems={'center'} gap={4} m={5}>
+          <Box
+            boxShadow={'xl'}
+            width={'260px'}
+            height={'260px'}
+            rounded={'lg'}
+            fontFamily={'lexendDeca'}
+          >
+            <Flex justifyContent={'center'} mt={8}>
+              <Avatar
+                src="/images/team/bhanu.jpeg"
+                size={'xl'}
+                justifyContent={'center'}
+              />
+            </Flex>
+            <Flex
+              flexDirection={'column'}
+              justifyContent={'center'}
+              alignItems={'center'}
+              p={5}
+            >
+              <Text textAlign={'center'} fontWeight={'semibold'} mb={4}>
+                Aswangga Bhanu Rizqullah
+              </Text>
+              <Text textAlign={'center'} fontWeight={'light'} fontSize={'sm'}>
+                Fullstack Web Developer
+              </Text>
+            </Flex>
+          </Box>
+
+          <Box
+            boxShadow={'xl'}
+            width={'260px'}
+            height={'260px'}
+            rounded={'lg'}
+            fontFamily={'lexendDeca'}
+          >
+            <Flex justifyContent={'center'} mt={8}>
+              <Avatar
+                src="/images/team/juna.jpg"
+                size={'xl'}
+                justifyContent={'center'}
+              />
+            </Flex>
+            <Flex
+              flexDirection={'column'}
+              justifyContent={'center'}
+              alignItems={'center'}
+              p={5}
+            >
+              <Text textAlign={'center'} fontWeight={'semibold'} mb={4}>
+                Christophel J.I. Soge
+              </Text>
+              <Text textAlign={'center'} fontWeight={'light'} fontSize={'sm'}>
+                Fullstack Web Developer
+              </Text>
+            </Flex>
+          </Box>
+
+          <Box
+            boxShadow={'xl'}
+            width={'260px'}
+            height={'260px'}
+            rounded={'lg'}
+            fontFamily={'lexendDeca'}
+          >
+            <Flex justifyContent={'center'} mt={8}>
+              <Avatar
+                src="/images/team/nicho.JPG"
+                size={'xl'}
+                justifyContent={'center'}
+              />
+            </Flex>
+            <Flex
+              flexDirection={'column'}
+              justifyContent={'center'}
+              alignItems={'center'}
+              p={5}
+            >
+              <Text textAlign={'center'} fontWeight={'semibold'} mb={4}>
+                Nicholas Sumanto
+              </Text>
+              <Text textAlign={'center'} fontWeight={'light'} fontSize={'sm'}>
+                Fullstack Web Developer
+              </Text>
+            </Flex>
+          </Box>
+        </Flex>
       </Stack>
-      <Stack padding={10}></Stack>
     </Wrapper>
   );
 }
