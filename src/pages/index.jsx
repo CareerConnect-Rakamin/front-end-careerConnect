5a14553ba6b1de2529111dbae10d80037f8f5385
Junatius <cjuniar0@gmail.com>
2023-12-02 06:02:42 +0800

    fix: adjust the page and searchbar function

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import { useEffect, useState } from 'react';
-import { Box, Flex, Heading, Text } from '@chakra-ui/react';
+import { Box, Flex, Heading, Text, useToast } from '@chakra-ui/react';
 import { getJobs } from '@/modules/fetch';
 import CardJobVacancy from '@/components/CardJobVacancy';
 import Wrapper from '@/components/Wrapper';
@@ -10,6 +10,7 @@ import { useRouter } from 'next/router';
 
 export default function Home() {
   const router = useRouter();
+  const toast = useToast();
   const [page, setPage] = useState(1);
   const [jobs, setJobs] = useState([]);
   const [lastPage, setLastPage] = useState(0);
@@ -17,17 +18,42 @@ export default function Home() {
   const fetchData = async () => {
     try {
       const job_type = router.query.job_type;
-      const response = await getJobs(page, job_type);
+      const search = router.query.search;
+      const response = await getJobs(page, search, job_type);
+
+      if (response.data.length === 0) {
+        return;
+      }
+
       setJobs(response.data);
-      setLastPage(response.pagination);
+      setLastPage(response.pagination.totalPages);
     } catch (error) {
       console.error('Error fetching jobs:', error);
+      toast({
+        title: 'Error',
+        description: 'Error fetching jobs. Please try again later.',
+        status: 'error',
+        duration: 3000,
+        isClosable: true,
+        position: 'top-center',
+        render: () => (
+          <Box
+            textAlign={'center'}
+            borderRadius={20}
+            color="white"
+            p={3}
+            bg="red.500"
+          >
+            No jobs found
+          </Box>
+        )
+      });
     }
   };
 
   useEffect(() => {
     fetchData();
-  }, [page, router.query.job_type]);
+  }, [page, router.query.search, router.query.job_type]);
 
   return (
     <Wrapper>
