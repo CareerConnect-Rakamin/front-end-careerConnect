32d31b9f4c59cfc6596a9a0c328c02ac5b2f3db1
bhanurz <aswanggab@gmail.com>
2023-12-03 01:36:15 +0700

    fix sidebar on jobseeker profile

--- src/components/SidebarJobseeker.jsx
+++ src/components/SidebarJobseeker.jsx
@@ -1,113 +1,69 @@
-import { Box, VStack, Text, Image, HStack } from '@chakra-ui/react';
-import Link from 'next/link';
+import { Box, Text, Image, Flex, Link, Divider } from '@chakra-ui/react';
+import { MdDeveloperBoard } from 'react-icons/md';
+import { FaBoxArchive } from 'react-icons/fa6';
 
 const baseURL = process.env.API_URL || 'http://localhost:3000/api/v1';
 
 const Sidebar = ({ dataUser }) => {
-  const image = dataUser?.photo_profile;
-  const id = dataUser?.id;
   if (!dataUser) {
     return <div>Loading...</div>;
   }
 
-  console.log(dataUser);
-  console.log(dataUser.photo_profile);
-
   return (
     <Box
       fontFamily={'lexendDeca'}
-      as="aside"
-      mt="21px"
-      ml="21px"
-      w="22.875em"
-      bg="#F5F5F5"
-      color="#0B1A2A"
-      borderRadius="20px"
-      pos={'sticky'}
-      h="30.688em"
-      boxShadow="7px 7px 5px rgba(0, 0, 0, 0.2)"
+      w="21em"
+      h="26em"
+      bg="gray.50"
+      rounded={'xl'}
+      boxShadow={'xl'}
+      p={5}
+      mx={5}
+      my={7}
     >
-      <VStack align="center" gap={0}>
-        {dataUser && (
-          <>
-            <Image
-              src={`${baseURL}/${dataUser.photo_profile}`}
-              alt={dataUser?.full_name}
-              borderRadius="20px"
-              w={125}
-              h={125}
-              mb={0}
-              mt={'47px'}
-            />
-            <Text fontSize="2xl" fontWeight="bold" mt={0} mb={0} gap={0}>
-              {dataUser?.full_name}
-            </Text>
-          </>
-        )}
-      </VStack>
-      <VStack align="left" mt={17} gap={0} ml={'12px'}>
-        <Text mb={0} fontSize="xl">
-          Menu Utama
-        </Text>
-        <Text
-          gap={0}
-          borderBottom="1.5px solid #2A5C91"
-          mt={0}
-          mb={0}
-          width="18em"
-        />
-      </VStack>
+      {dataUser && (
+        <Flex
+          direction={'column'}
+          justifyContent={'center'}
+          alignItems={'center'}
+          gap={2}
+          mt={10}
+        >
+          <Image
+            src={`${baseURL}/${dataUser.photo_profile}`}
+            alt={dataUser?.full_name}
+            borderRadius="20px"
+            w={125}
+            h={125}
+          />
+          <Text fontSize="2xl" fontWeight="bold">
+            {dataUser?.full_name}
+          </Text>
+        </Flex>
+      )}
+      <Text fontSize="xl" fontWeight={'semibold'} mt={6}>
+        Menu Utama
+      </Text>
+      <Divider
+        borderWidth={'2px'}
+        borderColor={'blue.300'}
+        rounded={'lg'}
+        w={'80%'}
+        my={3}
+      />
       {dataUser && (
         <Link href={`/profile/jobseeker/${dataUser.jobseekers_id}`}>
-          <HStack
-            align={'left'}
-            mt="8px"
-            mb={0}
-            gap={0}
-            w={'auto'}
-            pl={'12px'}
-            cursor={'pointer'}
-            _hover={{
-              bgColor: '#2A5C91',
-              color: '#F5F5F5',
-              '& img': {
-                filter: 'invert(100%)'
-              }
-            }}
-            pt={'11px'}
-            pb={'11px'}
-          >
-            <Image
-              src="/images/dashboard2.png"
-              w="22px"
-              h="22px"
-              color="blue.500"
-            />
-            <Text pl={'9px'}>Dashboard Pengguna</Text>
-          </HStack>
+          <Flex alignItems={'center'} gap={2} mb={3}>
+            <MdDeveloperBoard size={'24px'} />
+            <Text fontWeight={'light'}>Dashboard Pengguna</Text>
+          </Flex>
         </Link>
       )}
       <Link href={`/profile/jobseeker/jobs/${dataUser.jobseekers_id}`}>
-        <HStack
-          align={'left'}
-          mb={0}
-          gap={0}
-          w={'auto'}
-          pl={'12px'}
-          cursor={'pointer'}
-          _hover={{
-            bgColor: '#2A5C91',
-            color: '#F5F5F5',
-            '& img': {
-              filter: 'invert(100%)'
-            }
-          }}
-          pt={'11px'}
-          pb={'11px'}
-        >
-          <Image src="/images/job.png" w="22px" h="22px" color="blue.500" />
-          <Text pl={'9px'}>Data Apply Pekerjaan</Text>
-        </HStack>
+        <Flex alignItems={'center'} gap={2}>
+          <FaBoxArchive size={'24px'} />
+          <Text fontWeight={'light'}>Data Apply Pekerjaan</Text>
+        </Flex>
       </Link>
     </Box>
   );
